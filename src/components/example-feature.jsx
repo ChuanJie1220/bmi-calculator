f6afa596ce03d6da08b4d0c1e83e80d0ff00f526
tzq12344 <138437833+tzq12344@users.noreply.github.com>
2023-09-06 03:10:17 +0800

    bug is fixed

---
+++ src/components/example-feature.jsx
@@ -0,0 +1,14 @@
+function formatName(user) {
+    return user.lastName + ' ' + user.firstName;
+  }
+
+  const user = {
+    firstName: 'Zi Qin',
+    lastName: 'Thng'
+  };
+
+  const element = (
+<h1>
+      Hello, {formatName(user)}!
+</h1>
+  );
